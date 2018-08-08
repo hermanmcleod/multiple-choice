2d7876dffe17afc52c9c7e5bb4fe0168785008ff
Herman McLeod <hermanmcleod1@gmail.com>
2018-08-08 17:00:41 -0400

    tutorial completed

---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Question from '../../components/Question/Question';
+import QuestionCount from '../../components/QuestionCount/QuestionCount';
+import AnswerOption from '../../components/AnswerOption/AnswerOption';
+
+function Quiz(props) {
+
+	function renderAnswerOptions(key) {
+		return(
+			<AnswerOption
+			key={key.content}
+			answerContent={key.content}
+			answerType={key.type}
+			answer={props.answer}
+			questionId={props.questionId}
+			onAnswerSelected={props.onAnswerSelected} 
+			/>
+			)
+	}
+
+	return (
+		<div className='quiz'>
+			<QuestionCount
+			counter={props.questionId}
+			total={props.questionTotal}
+			/>
+			<Question
+			content={props.question} 
+			/>
+			<ul className="answerOptions">
+			{props.answerOptions.map(renderAnswerOptions)}
+			</ul>
+		</div>
+		)
+}
+
+export default Quiz;
